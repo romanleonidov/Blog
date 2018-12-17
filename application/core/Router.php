421d463265b93ef52dddea48bd9d9c97aa55159c
romanleonidov <roman.pozdnykow@mail.ru>
2018-12-17 22:40:24 +0300

    Класс Роутер

---
+++ application/core/Router.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace application\core;
+
+
+class Router
+{
+    public function __construct()
+    {
+        echo "я класс рутер";
+    }
+
+}
--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 require 'application/lib/Dev.php';
 
+use application\core\Router;
+
 spl_autoload_register(function ($class) {
-    $path = str_replace('\\', '/', '.php');
+    $path = str_replace('\\', '/', $class. '.php');
     if (file_exists($path)) {
         require $path;
     }
 });
+
+$router = new Router;
